bdcecb34f6aafed3cd542f6272ef642ebb0a7852
kevinfran <kevinfran@dd989199-f838-3d1c-c0bb-f9372f026deb>
2010-12-06 19:41:10 +0000

    Merging Social client into smartconnect

---
+++ trunk/smartconnect/apps/SocialClientService/src/com/android/smartconnect/apps/socialclient/DummyService.java
@@ -0,0 +1,52 @@
+package com.android.smartconnect.apps.socialclient;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DummyService extends Service {
+
+	private static final String TAG = "DUMMY SERVICE";
+	
+	Thread t1, t2, t3;
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void onCreate() {
+		Toast.makeText(this, "CREATED: Dummy Service", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onCreate()");
+	}
+	
+	@Override
+	public void onDestroy() {
+		Toast.makeText(this, "DESTROYED: Dummy Service", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onDestroy()");
+		/* Stop traffic */
+		t1.stop();
+		t2.stop();
+		t3.stop();
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startid) {
+		Toast.makeText(this, "STARTED: Dummy Service", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onStart()");
+	
+		/* Start Traffic in a separate thread */
+		//(new Thread(new TrafficMaker())).start();
+		t1 = new Thread(new TrafficMaker("http://www.google.com", 20000));
+		t2 = new Thread(new TrafficMaker("http://www.cs.ucsb.edu/~kevinfrancis", 5000));
+		t3 = new Thread(new TrafficMaker("http://www.yahoo.com", 10000));
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
---
+++ trunk/smartconnect/apps/SocialClientService/src/com/android/smartconnect/apps/socialclient/TrafficMaker.java
@@ -0,0 +1,62 @@
+package com.android.smartconnect.apps.socialclient;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TrafficMaker implements Runnable {
+	
+	private long intervalMilliseconds;
+	private URL url;
+	private LogHelper logHelper;
+	
+	public TrafficMaker() {
+		this("http://www.google.com", 5000);
+	}
+	
+	public TrafficMaker(String urlstring, long intervalMilliseconds) {
+		try {
+			this.url = new URL(urlstring);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		this.intervalMilliseconds = intervalMilliseconds;
+		this.logHelper = new LogHelper(urlstring);
+	}
+
+	public void run() {
+		int bytesRead = 0;
+		while (true) {
+			try {
+				logHelper.addLog("SEND");
+				bytesRead = wgetPage();
+				logHelper.addLog("RECV | " + bytesRead);
+				Thread.sleep(intervalMilliseconds);
+			} catch (Exception e) {
+				//TODO
+			}
+		}
+	}
+	
+	private int wgetPage() throws MalformedURLException, IOException {
+		
+		String urlData = "";
+		String inputLine;
+		
+		BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+					url.openStream()));
+
+		while ((inputLine = in.readLine()) != null)
+			urlData += inputLine + '\n';
+
+		in.close();
+		
+		return urlData.length();
+	}
+
+}
+	
