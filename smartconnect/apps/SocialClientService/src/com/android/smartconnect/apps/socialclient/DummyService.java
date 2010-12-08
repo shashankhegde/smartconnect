d284ab9a0ec3f612fbaa4abbe3704afa38b09969
hegde.shashank <hegde.shashank@dd989199-f838-3d1c-c0bb-f9372f026deb>
2010-12-08 01:46:53 +0000

    .

--- smartconnect/apps/SocialClientService/src/com/android/smartconnect/apps/socialclient/DummyService.java
+++ smartconnect/apps/SocialClientService/src/com/android/smartconnect/apps/socialclient/DummyService.java
@@ -1,16 +1,45 @@
 package com.android.smartconnect.apps.socialclient;
 
+import com.android.smartconnect.requestmanager.IRequestManager;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
+
+
 
 public class DummyService extends Service {
 
 	private static final String TAG = "DUMMY SERVICE";
 	
+	private IRequestManager reqManagerService;
+	private boolean connectedToReqManager = false;
+	
 	Thread t1, t2, t3, t4;
+	
+	ServiceConnection conn = new ServiceConnection() {
+		public void onServiceConnected(ComponentName name, IBinder boundService) {
+			reqManagerService = IRequestManager.Stub.asInterface((IBinder)boundService);
+			Log.i("INFO", "Service bound !");
+			if (reqManagerService == null) {
+				Log.i("INFO", "reqManager is null");
+			}
+			connectedToReqManager = true;
+			Log.i("INFO", "connectedToReqManager ? => " + connectedToReqManager);
+		}
+		
+		public void onServiceDisconnected(ComponentName arg0) {
+			Log.i("INFO", "Service Unbound ");
+			connectedToReqManager = false;
+		}
+	};
+	
+	public boolean getConnectedState() {
+		return connectedToReqManager;
+	}
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -22,13 +51,24 @@ public class DummyService extends Service {
 	public void onCreate() {
 		Toast.makeText(this, "CREATED: Dummy Service", Toast.LENGTH_LONG).show();
 		Log.d(TAG, "onCreate()");
+		
+		// Start the Request Manager Service also
+		Intent intent = new Intent();
+		intent.setClassName("com.android.smartconnect.requestmanager",
+					"com.android.smartconnect.requestmanager.RequestManagerService");
+		startService(intent);
+		
+		// Service Connection
+		
+		// Bind service
+		bindService(intent, conn, 0);
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		Toast.makeText(this, "DESTROYED: Dummy Service", Toast.LENGTH_LONG).show();
 		Log.d(TAG, "onDestroy()");
-		/* Stop traffic */
+		// Stop traffic 
 		t1.stop();
 		t2.stop();
 		t3.stop();
@@ -37,19 +77,44 @@ public class DummyService extends Service {
 	
 	@Override
 	public void onStart(Intent intent, int startid) {
+		
 		Toast.makeText(this, "STARTED: Dummy Service", Toast.LENGTH_LONG).show();
 		Log.d(TAG, "onStart()");
+		
+		Log.i("INFO", "Starting threads");
+		if (reqManagerService == null)
+			Log.i("INFO", "reqManagerService is null");
 	
 		/* Start Traffic in a separate thread */
 		//(new Thread(new TrafficMaker())).start();
-		t1 = new Thread(new TrafficMaker("http://www.google.com", 120000, 10000));	// every 2 min, variance = 10 secs
-		t2 = new Thread(new TrafficMaker("http://www.cs.ucsb.edu/", 300000, 30000)); // every 5 min, variance = 30 secs
-		t3 = new Thread(new TrafficMaker("http://www.nytimes.com", 300000, 10000));	// every 5 min, variance = 10 secs
-		t4 = new Thread(new TrafficMaker("http://www.reddit.com/.rss", 900000, 60000));	// every 15 min, variance = 60 secs
-		
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
+
+		(new Thread(new TrafficKickStarter())).start();
 	}
+	
+	private class TrafficKickStarter implements Runnable {
+
+		public void run() {
+			// TODO Auto-generated method stub
+			while (connectedToReqManager == false) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+				
+			t1 = new Thread(new TrafficMaker("http://www.google.com", 120000, 10000,reqManagerService));	// every 2 min, variance = 10 secs
+			t2 = new Thread(new TrafficMaker("http://www.cs.ucsb.edu/", 300000, 30000,reqManagerService)); // every 5 min, variance = 30 secs
+			t3 = new Thread(new TrafficMaker("http://www.nytimes.com", 300000, 10000,reqManagerService));	// every 5 min, variance = 10 secs
+			t4 = new Thread(new TrafficMaker("http://www.reddit.com/.rss", 900000, 60000,reqManagerService));	// every 15 min, variance = 60 secs
+
+			t1.start();
+			t2.start();
+			t3.start();
+			t4.start();
+		}
+	}
+	
+	
 }
--- smartconnect/apps/SocialClientService/src/com/android/smartconnect/apps/socialclient/TrafficMaker.java
+++ smartconnect/apps/SocialClientService/src/com/android/smartconnect/apps/socialclient/TrafficMaker.java
@@ -10,19 +10,27 @@ import java.security.Timestamp;
 import java.util.Date;
 import java.util.Random;
 
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import com.android.smartconnect.requestmanager.IRequestManager;
+import com.android.smartconnect.requestmanager.RequestCallback;
+import com.android.smartconnect.requestmanager.RequestManagerService;
+
 public class TrafficMaker implements Runnable {
 	
 	private long intervalMilliseconds;
 	private URL url;
 	private LogHelper logHelper;
-	private long iVariance = 0;
+	private IRequestManager reqManagerService;
+	private long iVariance;
 	private Random iRandomNumberGenerator;
 	
-	public TrafficMaker() {
-		this("http://www.google.com", 5000, 0);
+	public TrafficMaker(IRequestManager service) {
+		this("http://www.google.com", 5000, 0,service);
 	}
 	
-	public TrafficMaker(String urlstring, long intervalMilliseconds, long aVariance) {
+	public TrafficMaker(String urlstring, long intervalMilliseconds, long aVariance, IRequestManager service) {
 		try {
 			this.url = new URL(urlstring);
 		} catch (MalformedURLException e) {
@@ -30,26 +38,52 @@ public class TrafficMaker implements Runnable {
 		}
 		this.intervalMilliseconds = intervalMilliseconds;
 		this.logHelper = new LogHelper(urlstring);
+		this.reqManagerService = service;
 		this.iVariance = aVariance;
 		iRandomNumberGenerator = new Random((new Date()).getTime());
 	}
+	
+	class IncomingHandler implements RequestCallback {// TODO Auto-generated method stub
 
+		public void onDataReceived(String arg0) throws RemoteException {
+			logHelper.addLog("RECV | " + arg0.length() + ";");
+		}
+
+		public IBinder asBinder() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	    
+	}
+
+	IncomingHandler iIncomingHandler = new IncomingHandler();
+	
 	public void run() {
+		
 		int bytesRead = 0;
 		while (true) {
 			try {
+				String strUrl = url.toExternalForm();
 				logHelper.addLog("SEND");
-				bytesRead = wgetPage();
-				logHelper.addLog("RECV | " + bytesRead);
+				//bytesRead = wgetPage();
+				//logHelper.addLog("RECV | " + bytesRead);
+
+				reqManagerService.GetData(strUrl, iIncomingHandler);
+
 				int extraDelay = iRandomNumberGenerator.nextInt((int) iVariance/2);
 				if( iRandomNumberGenerator.nextBoolean() == false )
 					extraDelay *= -1;
 				
 				Thread.sleep(intervalMilliseconds+extraDelay);
 			} catch (Exception e) {
-				//TODO
+				logHelper.addLog("++Exception: " + e.toString());
+				if (reqManagerService == null) {
+					logHelper.addLog("reqManager is null");
+				}
+				break;
 			}
 		}
+
 	}
 	
 	private int wgetPage() throws MalformedURLException, IOException {
@@ -63,7 +97,7 @@ public class TrafficMaker implements Runnable {
 
 		while ((inputLine = in.readLine()) != null)
 			urlData += inputLine + '\n';
-
+		
 		in.close();
 		
 		return urlData.length();
